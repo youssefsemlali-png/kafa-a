984b405961ad3f0dc42274c83c8975720a7ae7b0
youssefsemlali-png <youssef.semlali@usmba.ac.ma>
2026-09-26 01:29:44 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // خدمة العامل (Service Worker) - تطبيق إمتحانات وزارة العدل
-const CACHE_NAME = 'exam-app-cache-v27';
+const CACHE_NAME = 'exam-app-cache-v28';
 const APP_SHELL = [
   './',
   './index.html',
